f33ae9771adc45c8e60259876178e1066e99dd7c
GethinDavies1990 <gethindavies1990@gmail.com>
2023-04-07 20:11:34 +0000

    folder restructure for blueprint

---
+++ app2.py
@@ -0,0 +1,10 @@
+import os
+from binge_reviews import create_app
+
+# Create an app
+app = create_app()
+
+# Run the app
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"), port=int(
+        os.environ.get("PORT")), debug=True)
