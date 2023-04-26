79851d689ee83890de0668b441583d6607bcf23d
GethinDavies1990 <gethindavies1990@gmail.com>
2023-04-26 18:27:17 +0000

    feat: add all error functions

--- binge_reviews/errors/routes.py
+++ binge_reviews/errors/routes.py
@@ -12,3 +12,39 @@ def error_404(error: object) -> object:
     :return render_template of 404.html
     """
     return render_template('errors/404.html', error=error), 404
+
+
+@app.errorhandler(401)
+def error_401(error: object) -> object:
+    """
+    Render 401.html template in case of 401 error
+    :return render_template of 401.html
+    """
+    return render_template('errors/401.html', error=error), 401
+
+
+@app.errorhandler(400)
+def error_400(error: object) -> object:
+    """
+    Render 400.html template in case of 400 error
+    :return render_template of 400.html
+    """
+    return render_template('errors/400.html', error=error), 400
+
+
+@app.errorhandler(405)
+def error_405(error: object) -> object:
+    """
+    Render 405.html template in case of 405 error
+    :return render_template of 405.html
+    """
+    return render_template('errors/405.html', error=error), 405
+
+
+@app.errorhandler(500)
+def error_500(error: object) -> object:
+    """
+    Render 500.html template in case of 500 error
+    :return render_template of 500.html
+    """
+    return render_template('errors/500.html', error=error), 500
