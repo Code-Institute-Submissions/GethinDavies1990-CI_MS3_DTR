d8349c644906a1f1bdffc58e7ea01e3bbf46d9bd
GethinDavies1990 <gethindavies1990@gmail.com>
2023-05-04 18:12:49 +0000

    testing: commented out javascript code

---
+++ TESTING.md
@@ -0,0 +1,4 @@
+# Testing
+
+The testing performed in this unit is described in this testing readme: 
+1. Manual testing using emulators and real devices. 
--- binge_reviews/static/js/scripts.js
+++ binge_reviews/static/js/scripts.js
@@ -1,19 +1,19 @@
-const username = document.getElementById("username");
-const password = document.getElementById("password");
-const firstName = document.getElementById("first_name");
-const lastName = document.getElementById("last_name");
-const favFilm = document.getElementById("fav_film");
-const image = document.getElementById("upload_image");
-const form = document.getElementById("form");
-const errorElement = document.getElementsByClassName('error')
+// const username = document.getElementById("username");
+// const password = document.getElementById("password");
+// const firstName = document.getElementById("first_name");
+// const lastName = document.getElementById("last_name");
+// const favFilm = document.getElementById("fav_film");
+// const image = document.getElementById("upload_image");
+// const form = document.getElementById("form");
+// const errorElement = document.getElementsByClassName('error')
 
-form.addEventListener("submit", (e) => {
-  let messages = [];
-  if (username.value === "" || username.value == null) {
-    messages.push("Username is required");
-  }
-  if (messages.length > 0) {
+// form.addEventListener("submit", (e) => {
+//   let messages = [];
+//   if (username.value === "" || username.value == null) {
+//     messages.push("Username is required");
+//   }
+//   if (messages.length > 0) {
 
-  e.preventDefault()
-  errorElement.innerText = messages.join(',')
-});
+//   e.preventDefault()
+//   errorElement.innerText = messages.join(',')
+// });
