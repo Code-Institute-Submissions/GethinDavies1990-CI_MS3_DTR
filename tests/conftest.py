d1459bd6bc54e84869aeaffe206b06b594f6808c
GethinDavies1990 <gethindavies1990@gmail.com>
2023-05-11 10:25:56 +0000

    unti_testing: add register unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,37 @@
+import pytest
+from binge_reviews import create_app
+import mongomock
 
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+
+@pytest.fixture(scope='session')
+def mongo_client():
+    return mongomock.MongoClient()
+
+
+@pytest.fixture(scope='function')
+def mongo_database(mongo_client):
+    return mongo_client.db_name
