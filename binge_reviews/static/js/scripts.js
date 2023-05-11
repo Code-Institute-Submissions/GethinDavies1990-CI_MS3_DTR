d94014aaa89f6a3ca72dbd5153b00ee9e97a21b9
GethinDavies1990 <gethindavies1990@gmail.com>
2023-05-11 14:29:09 +0000

    feat: add server side validation for register form

--- binge_reviews/static/js/scripts.js
+++ binge_reviews/static/js/scripts.js
@@ -1,6 +1,9 @@
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const password = document.getElementById('password');
+const firstName = document.getElementById('first_name')
+const lastName = document.getElementById('last_name')
+const favFilm = document.getElementById('fav_film')
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -28,28 +31,72 @@ const setSuccess = element => {
 };
 
 const validateInputs = () => {
-    const usernameValue = username.value.trim();
-    const passwordValue = password.value.trim();
+    let isValid = true;
 
-    if(usernameValue === '' ) {
+    // Check username and password fields for both forms
+    if (username) {
+      const usernameValue = username.value.trim();
+      if (usernameValue === '') {
         setError(username, 'Username is required');
-        return false;
-    } else if (usernameValue.length < 5 ) {
+        isValid = false;
+      } else if (usernameValue.length < 5) {
         setError(username, 'Username must be at least 5 characters.');
-        return false;
-    } else {
+        isValid = false;
+      } else {
         setSuccess(username);
+      }
     }
-
-    if(passwordValue === '') {
+    if (password) {
+      const passwordValue = password.value.trim();
+      if (passwordValue === '') {
         setError(password, 'Password is required');
-        return false;
-    } else if (passwordValue.length < 5 ) {
+        isValid = false;
+      } else if (passwordValue.length < 5) {
         setError(password, 'Password must be at least 5 characters.');
-        return false;
-    } else {
+        isValid = false;
+      } else {
         setSuccess(password);
+      }
     }
 
-    return true;
-};
+    // Check additional fields for registration form
+    if (firstName) {
+      const firstNameValue = firstName.value.trim();
+      if (firstNameValue === '') {
+        setError(firstName, 'First name is required');
+        isValid = false;
+      } else if (firstNameValue.length < 1) {
+        setError(firstName, 'First name must be at least 1 character.');
+        isValid = false;
+      } else {
+        setSuccess(firstName);
+      }
+    }
+    if (lastName) {
+      const lastNameValue = lastName.value.trim();
+      if (lastNameValue === '') {
+        setError(lastName, 'Last name is required');
+        isValid = false;
+      } else if (lastNameValue.length < 1) {
+        setError(lastName, 'Last name must be at least 1 character.');
+        isValid = false;
+      } else {
+        setSuccess(lastName);
+      }
+    }
+    if (favFilm) {
+      const favFilmValue = favFilm.value.trim();
+      if (favFilmValue === '') {
+        setError(favFilm, 'Favorite film is required');
+        isValid = false;
+      } else if (favFilmValue.length < 1) {
+        setError(favFilm, 'Favorite film must be at least 1 character.');
+        isValid = false;
+      } else {
+        setSuccess(favFilm);
+      }
+    }
+
+    return isValid;
+  };
+
