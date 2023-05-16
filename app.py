35169e44dbbdf3c5680c0809fea4ef5a9ba97904
GethinDavies1990 <gethindavies1990@gmail.com>
2023-05-16 14:28:33 +0000

    set debug value to false, this was accepted by code institute after submission.

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ app = create_app()
 # Run the app
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), port=int(
-        os.environ.get("PORT")), debug=True)
+        os.environ.get("PORT")), debug=False)
